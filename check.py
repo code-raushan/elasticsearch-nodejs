e7126768e3206ec4ab9c7cb6b8c9d697dcf67b70
code-raushan <code-raushan@users.noreply.github.com>
2024-06-05 14:36:45 +0530

    pgsync setup

---
+++ check.py
@@ -0,0 +1,11 @@
+import os
+
+print(os.getenv('ELASTIC_PASSWORD'))
+print(os.getenv('ELASTICSEARCH_SCHEME'))
+print(os.getenv('ELASTICSEARCH_HOST'))
+print(os.getenv('ELASTICSEARCH_PORT'))
+print(os.getenv('ELASTICSEARCH_USER'))
+print(os.getenv('ELASTICSEARCH_PASSWORD'))
+print(os.getenv('ELASTICSEARCH_USE_SSL'))
+print(os.getenv('ELASTICSEARCH_VERIFY_CERTS'))
+print(os.getenv('ELASTICSEARCH_CA_CERTS'))
